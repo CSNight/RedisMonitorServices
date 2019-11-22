79ed0817404c59c5bc5e68505574092ef179a2c8
CSNight <923739637@qq.com>
2019-11-22 15:05:12 +0800

    add batch log pool config

--- src/main/java/com/csnight/redis/monitor/context/SpringContextEvent.java
+++ src/main/java/com/csnight/redis/monitor/context/SpringContextEvent.java
@@ -1,11 +1,14 @@
 package com.csnight.redis.monitor.context;
 
+import com.csnight.redis.monitor.aop.LogAsync;
+import com.csnight.redis.monitor.aop.LogAsyncPool;
 import com.csnight.redis.monitor.quartz.JobFactory;
 import com.csnight.redis.monitor.utils.ReflectUtils;
 import org.quartz.SchedulerException;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.boot.context.event.ApplicationStartedEvent;
+import org.springframework.boot.context.event.ApplicationStartingEvent;
 import org.springframework.context.ApplicationEvent;
 import org.springframework.context.ApplicationListener;
 import org.springframework.context.event.ContextClosedEvent;
@@ -14,12 +17,16 @@ import org.springframework.web.context.support.ServletRequestHandledEvent;
 
 public class SpringContextEvent implements ApplicationListener {
     private static Logger _log = LoggerFactory.getLogger(SpringContextEvent.class);
+    private LogAsyncPool logAsyncPool=LogAsyncPool.getIns();
 
     @Override
     public void onApplicationEvent(ApplicationEvent applicationEvent) {
         if (applicationEvent instanceof ApplicationStartedEvent) {
-            System.out.println("Start Complete!");
+            logAsyncPool.initBean();
+            logAsyncPool.StartLogPool();
+            _log.info("RMS Server Start Complete!");
         } else if (applicationEvent instanceof ContextClosedEvent) {
+            logAsyncPool.StopLogPool();
             ReflectUtils.getBean(JobFactory.class).DeleteAllJob();
             try {
                 ReflectUtils.getBean(SchedulerFactoryBean.class).getScheduler().shutdown(true);
