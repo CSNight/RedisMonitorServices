da23ac8b65142e273a7ca9607e17cca8744f3c79
S CH <923739637@qq.com>
2019-11-28 23:01:55 +0800

    add config exception

---
+++ src/main/java/csnight/redis/monitor/exception/ConfigException.java
@@ -0,0 +1,14 @@
+package csnight.redis.monitor.exception;
+
+public class ConfigException extends Exception {
+    public ConfigException() {
+    }
+
+    public ConfigException(String msg) {
+        super("Redis Config Error:" + msg);
+    }
+
+    public ConfigException(String msg, Throwable t) {
+        super("Redis Config Error:" + msg, t);
+    }
+}
