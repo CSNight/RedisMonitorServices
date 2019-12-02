dfdf0e325ca923663913328ffdf2f65b363c9a4d
CSNight <923739637@qq.com>
2019-12-02 17:33:57 +0800

    add user ids query

--- src/main/java/csnight/redis/monitor/busi/sys/exp/UserQueryExp.java
+++ src/main/java/csnight/redis/monitor/busi/sys/exp/UserQueryExp.java
@@ -2,9 +2,22 @@ package csnight.redis.monitor.busi.sys.exp;
 
 import csnight.redis.monitor.db.blurry.Query;
 
+import java.util.HashSet;
+import java.util.Set;
+
 public class UserQueryExp {
+    @Query(type = Query.Type.IN, propName = "id")
+    private Set<String> ids = new HashSet<>();
     @Query(blurry = "username,phone")
     private String blurry;
+
+    public Set<String> getIds() {
+        return ids;
+    }
+
+    public void setIds(Set<String> ids) {
+        this.ids = ids;
+    }
 
     public String getBlurry() {
         return blurry;
