c3fa0df07e36d8408b558b5aec77c616f524eabe
CSNight <923739637@qq.com>
2019-12-05 14:50:26 +0800

    logout close all instance belong to user

--- src/main/java/csnight/redis/monitor/auth/handler/SignOutHandler.java
+++ src/main/java/csnight/redis/monitor/auth/handler/SignOutHandler.java
@@ -1,5 +1,10 @@
 package csnight.redis.monitor.auth.handler;
 
+import csnight.redis.monitor.db.jpa.RmsInstance;
+import csnight.redis.monitor.db.jpa.SysUser;
+import csnight.redis.monitor.db.repos.RmsInsRepository;
+import csnight.redis.monitor.db.repos.SysUserRepository;
+import csnight.redis.monitor.redis.pool.MultiRedisPool;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.security.core.Authentication;
@@ -10,6 +15,7 @@ import javax.annotation.Resource;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.util.Date;
+import java.util.List;
 import java.util.Map;
 
 @Component
@@ -17,6 +23,10 @@ public class SignOutHandler implements LogoutHandler {
     private static Logger _log = LoggerFactory.getLogger(SignOutHandler.class);
     @Resource
     private LoginSuccessHandler successHandler;
+    @Resource
+    private SysUserRepository userRepository;
+    @Resource
+    private RmsInsRepository insRepository;
 
     @Override
     public void logout(HttpServletRequest request, HttpServletResponse response, Authentication authentication) {
@@ -27,14 +37,30 @@ public class SignOutHandler implements LogoutHandler {
                 remove_key = ent.getKey();
             }
         }
-        //TODO 查询persisit 表 获取remember-me 记录 综合判断
         if (!remove_key.equals("")) {
             successHandler.getLoginUserList().remove(remove_key);
+            ShutdownRedisDbAndJobs(remove_key);
         }
         _log.info(remove_key + ":账户登出成功" + new Date());
     }
 
-    private void InitializeRedisDbAndJobs() {
-        //TODO 增加用户数据库连接逻辑及定时任务终止逻辑
+    private void ShutdownRedisDbAndJobs(String username) {
+        try {
+            SysUser user = userRepository.findByUsername(username);
+            if (user != null) {
+                boolean st = MultiRedisPool.getInstance().shutdownUserPools(user.getId());
+                if (st) {
+                    List<RmsInstance> instances = insRepository.findByUserId(user.getId());
+                    for (RmsInstance ins : instances) {
+                        ins.setState(false);
+                    }
+                    insRepository.saveAll(instances);
+                }
+                //TODO 停止关联定时任务
+            }
+        } catch (Exception ex) {
+            ex.printStackTrace();
+            _log.error(ex.getMessage());
+        }
     }
 }
