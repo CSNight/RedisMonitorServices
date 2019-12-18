b6ce92312ddd8133f4fe1a0226f8694b70349f8d
CSNight <923739637@qq.com>
2019-12-18 17:46:26 +0800

    add subscribe start msg

--- src/main/java/csnight/redis/monitor/msg/entity/PubSubEntity.java
+++ src/main/java/csnight/redis/monitor/msg/entity/PubSubEntity.java
@@ -44,7 +44,13 @@ public class PubSubEntity extends JedisPubSub {
 
     // 初始化订阅时候的处理
     public void onSubscribe(String channel, int subscribedChannels) {
-        System.out.println(channel + "=" + subscribedChannels);
+        String msg = "Reading messages... (press Ctrl-C to quit)\r\n";
+        msg += "1) subscribe on" + channel + "\r\n";
+        msg += "2) channel count:" + subscribedChannels + "\r\n";
+        msg += "3) timestamp:" + System.currentTimeMillis() + "\r\n";
+        WssResponseEntity wre = new WssResponseEntity(ResponseMsgType.PUBSUB, msg);
+        wre.setAppId(appId);
+        WebSocketServer.getInstance().send(JSONObject.toJSONString(wre), ch);
     }
 
     // 取消订阅时候的处理
@@ -54,7 +60,13 @@ public class PubSubEntity extends JedisPubSub {
 
     // 初始化按表达式的方式订阅时候的处理
     public void onPSubscribe(String pattern, int subscribedChannels) {
-        System.out.println(pattern + "=" + subscribedChannels);
+        String msg = "Reading messages... (press Ctrl-C to quit)\r\n";
+        msg += "1) psubscribe on " + pattern + "\r\n";
+        msg += "2) channel count:" + subscribedChannels + "\r\n";
+        msg += "3) timestamp:" + System.currentTimeMillis() + "\r\n";
+        WssResponseEntity wre = new WssResponseEntity(ResponseMsgType.PUBSUB, msg);
+        wre.setAppId(appId);
+        WebSocketServer.getInstance().send(JSONObject.toJSONString(wre), ch);
     }
 
     // 取消按表达式的方式订阅时候的处理
