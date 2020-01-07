a9d8218df2d7f3262ed469f111b058e6e3b75291
S CH <923739637@qq.com>
2020-01-07 20:04:00 +0800

    add key check delete and expire

--- src/main/java/csnight/redis/monitor/redis/data/KeyOperator.java
+++ src/main/java/csnight/redis/monitor/redis/data/KeyOperator.java
@@ -2,6 +2,7 @@ package csnight.redis.monitor.redis.data;
 
 import com.csnight.jedisql.*;
 import csnight.redis.monitor.redis.pool.RedisPoolInstance;
+import csnight.redis.monitor.rest.rms.dto.KeyEntDto;
 import csnight.redis.monitor.rest.rms.dto.KeyScanDto;
 import csnight.redis.monitor.utils.IdentifyUtils;
 
@@ -12,7 +13,7 @@ import java.util.*;
  * @description
  * @since 2020/1/6 09:47
  */
-public class KeyScanner {
+public class KeyOperator {
     public Map<String, Object> ScanKeys(RedisPoolInstance pool, KeyScanDto dto) {
         Map<String, Object> res = new HashMap<>();
         List<Map<String, Object>> keySet = new ArrayList<>();
@@ -50,14 +51,12 @@ public class KeyScanner {
                 }
                 keySet.add(joKey);
             }
-
             pipeline.close();
             ttlPipeRes.clear();
             typePipeRes.clear();
             keys.clear();
             res.put("cursor", keysRes.getCursor());
             res.put("keys", GetKeySize(jediSQL, keySet));
-
         } finally {
             jediSQL.select(0);
             pool.close(jid);
@@ -118,15 +117,7 @@ public class KeyScanner {
             String key = kt.get("key").toString();
             pipeRes.put(key, p.strlen(key));
         }
-        long s = System.currentTimeMillis();
-        p.sync();
-        long e = System.currentTimeMillis();
-        System.out.println(e - s);
-        for (Map<String, Object> kt : strArr) {
-            String key = kt.get("key").toString();
-            long size = pipeRes.get(key).get();
-            kt.put("size", size);
-        }
+        GetResponse(strArr, pipeRes, p);
         p.close();
         pipeRes.clear();
         pipeRes = null;
@@ -139,12 +130,7 @@ public class KeyScanner {
             String key = kt.get("key").toString();
             pipeRes.put(key, p.llen(key));
         }
-        p.sync();
-        for (Map<String, Object> kt : listArr) {
-            String key = kt.get("key").toString();
-            long size = pipeRes.get(key).get();
-            kt.put("size", size);
-        }
+        GetResponse(listArr, pipeRes, p);
         p.close();
         pipeRes.clear();
         pipeRes = null;
@@ -157,15 +143,19 @@ public class KeyScanner {
             String key = kt.get("key").toString();
             pipeRes.put(key, p.scard(key));
         }
+        GetResponse(setArr, pipeRes, p);
+        p.close();
+        pipeRes.clear();
+        pipeRes = null;
+    }
+
+    private void GetResponse(List<Map<String, Object>> setArr, Map<String, Response<Long>> pipeRes, Pipeline p) {
         p.sync();
         for (Map<String, Object> kt : setArr) {
             String key = kt.get("key").toString();
             long size = pipeRes.get(key).get();
             kt.put("size", size);
         }
-        p.close();
-        pipeRes.clear();
-        pipeRes = null;
     }
 
     private void getZSetSizes(JediSQL jediSQL, List<Map<String, Object>> zsetArr) {
@@ -175,12 +165,7 @@ public class KeyScanner {
             String key = kt.get("key").toString();
             pipeRes.put(key, p.zcount(key, "-inf", "+inf"));
         }
-        p.sync();
-        for (Map<String, Object> kt : zsetArr) {
-            String key = kt.get("key").toString();
-            long size = pipeRes.get(key).get();
-            kt.put("size", size);
-        }
+        GetResponse(zsetArr, pipeRes, p);
         p.close();
         pipeRes.clear();
         pipeRes = null;
@@ -193,40 +178,80 @@ public class KeyScanner {
             String key = kt.get("key").toString();
             pipeRes.put(key, p.hlen(key));
         }
-        p.sync();
-        for (Map<String, Object> kt : hashArr) {
-            String key = kt.get("key").toString();
-            long size = pipeRes.get(key).get();
-            kt.put("size", size);
-        }
+        GetResponse(hashArr, pipeRes, p);
         p.close();
         pipeRes.clear();
         pipeRes = null;
     }
 
-    private void GetKeyValue(JediSQL jediSQL, Map<String, Object> kt) {
-        String key = kt.get("key").toString();
-        switch (kt.get("type").toString()) {
-            case "string":
-                String kv = jediSQL.get(key);
-                kt.put("val", kv);
-                break;
-            case "hash":
-                Map<String, String> table = jediSQL.hgetAll(key);
-                kt.put("val", table);
-                break;
-            case "zset":
-                Set<Tuple> zset = jediSQL.zrangeByScoreWithScores(key, "-inf", "+inf");
-                kt.put("val", zset);
-                break;
-            case "set":
-                Set<String> set = jediSQL.smembers(key);
-                kt.put("val", set);
-                break;
-            case "list":
-                List<String> list = jediSQL.lrange(key, 0, -1);
-                kt.put("val", list);
-                break;
+    public Map<String, Object> GetKeyValue(RedisPoolInstance pool, KeyEntDto kt) {
+        String jid = IdentifyUtils.getUUID();
+        JediSQL jediSQL = pool.getJedis(jid);
+        jediSQL.select(kt.getDb());
+        String key = kt.getKeyName();
+        Map<String, Object> keyVal = new HashMap<>();
+        try {
+            switch (kt.getType()) {
+                default:
+                case "string":
+                    String kv = jediSQL.get(key);
+                    keyVal.put(key, kv);
+                    break;
+                case "hash":
+                    Map<String, String> table = jediSQL.hgetAll(key);
+                    keyVal.put(key, table);
+                    break;
+                case "zset":
+                    Set<Tuple> zset = jediSQL.zrangeByScoreWithScores(key, "-inf", "+inf");
+                    keyVal.put(key, zset);
+                    break;
+                case "set":
+                    Set<String> set = jediSQL.smembers(key);
+                    keyVal.put(key, set);
+                    break;
+                case "list":
+                    List<String> list = jediSQL.lrange(key, 0, -1);
+                    keyVal.put(key, list);
+                    break;
+            }
+        } finally {
+            jediSQL.select(0);
+            pool.close(jid);
+            System.gc();
         }
+        return keyVal;
+    }
+
+    public boolean SetKeyExpire(RedisPoolInstance pool, KeyEntDto kt) {
+        String jid = IdentifyUtils.getUUID();
+        JediSQL jediSQL = pool.getJedis(jid);
+        jediSQL.select(kt.getDb());
+        boolean res;
+        try {
+            String key = kt.getKeyName();
+            long suc = jediSQL.pexpire(key, kt.getTtl());
+            res = suc != 0;
+        } finally {
+            jediSQL.select(0);
+            pool.close(jid);
+            System.gc();
+        }
+        return res;
+    }
+
+    public boolean DeleteKey(RedisPoolInstance pool, KeyEntDto kt) {
+        String jid = IdentifyUtils.getUUID();
+        JediSQL jediSQL = pool.getJedis(jid);
+        jediSQL.select(kt.getDb());
+        boolean res;
+        try {
+            long delCount = jediSQL.del(kt.getKeys().toArray(new String[0]));
+            res = delCount != 0;
+        } finally {
+            jediSQL.select(0);
+            pool.close(jid);
+            System.gc();
+        }
+        return res;
     }
 }
