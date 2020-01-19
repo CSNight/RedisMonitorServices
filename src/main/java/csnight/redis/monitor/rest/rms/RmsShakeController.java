bf78784f850eb0a010290c62c1e39ec4fb5a1eb6
chensi <chensi@thtf.com.cn>
2020-01-19 15:03:58 +0800

    add redis shake api

--- src/main/java/csnight/redis/monitor/rest/rms/RmsShakeController.java
+++ src/main/java/csnight/redis/monitor/rest/rms/RmsShakeController.java
@@ -8,12 +8,11 @@ import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiOperation;
 import org.springframework.http.HttpStatus;
 import org.springframework.security.access.prepost.PreAuthorize;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import javax.annotation.Resource;
 import javax.validation.Valid;
+import java.io.IOException;
 
 /**
  * @author csnight
@@ -28,10 +27,34 @@ public class RmsShakeController {
     private RmsDataDumpImpl rmsDataDump;
 
     @LogAsync(module = "DUMP", auth = "DUMP_ADD_CONF")
-    @ApiOperation("检索实例库键")
+    @ApiOperation("生成配置文件")
     @PreAuthorize("hasAuthority('DUMP_ADD_CONF')")
     @RequestMapping(value = "/shakeConf", method = RequestMethod.POST)
-    public RespTemplate GetInstanceKeys(@Valid DumpDto dto) {
+    public RespTemplate GenerateShakeConf(@Valid @RequestBody DumpDto dto) throws IOException {
         return new RespTemplate(HttpStatus.OK, rmsDataDump.NewShakeConf(dto));
+    }
+
+    @LogAsync(module = "DUMP", auth = "DUMP_DEL_RECORD")
+    @ApiOperation("删除操作记录")
+    @PreAuthorize("hasAuthority('DUMP_DEL_RECORD')")
+    @RequestMapping(value = "/shakeRecord/{cid}", method = RequestMethod.DELETE)
+    public RespTemplate DeleteShakeRecord(@PathVariable String cid) {
+        return new RespTemplate(HttpStatus.OK, rmsDataDump.DeleteRecord(cid));
+    }
+
+    @LogAsync(module = "DUMP", auth = "DUMP_GET_RECORD")
+    @ApiOperation("查询操作记录")
+    @PreAuthorize("hasAuthority('DUMP_GET_RECORD')")
+    @RequestMapping(value = "/shakeRecords", method = RequestMethod.GET)
+    public RespTemplate GetShakeRecords() {
+        return new RespTemplate(HttpStatus.OK, rmsDataDump.GetAll());
+    }
+
+    @LogAsync(module = "DUMP", auth = "DUMP_GET_RECORD")
+    @ApiOperation("根据用户查询操作记录")
+    @PreAuthorize("hasAuthority('DUMP_GET_RECORD')")
+    @RequestMapping(value = "/shakeRecordsByUser", method = RequestMethod.GET)
+    public RespTemplate GetShakeRecordsByUser() {
+        return new RespTemplate(HttpStatus.OK, rmsDataDump.GetByUser());
     }
 }
