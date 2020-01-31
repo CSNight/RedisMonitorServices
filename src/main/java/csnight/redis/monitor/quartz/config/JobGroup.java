d5aee7d479d486f4d8f6b35959683ec688ad3f1b
CSNight <923739637@qq.com>
2020-01-31 21:42:05 +0800

    add job  group enum

--- src/main/java/csnight/redis/monitor/quartz/config/JobGroup.java
+++ src/main/java/csnight/redis/monitor/quartz/config/JobGroup.java
@@ -1,12 +1,38 @@
 package csnight.redis.monitor.quartz.config;
 
+import com.csnight.jedisql.util.SafeEncoder;
+
+import java.util.Arrays;
+
 public enum JobGroup {
+    UNKNOWN(-1),
     STATISTIC(1),
-    CUSTOM(100);
+    EXECUTION(2),
+    ERROR(400);
+    private final byte[] raw = SafeEncoder.encode(this.name());
     private int ct;
 
     JobGroup(int ct) {
         this.ct = ct;
     }
 
+    public byte[] getRaw() {
+        return this.raw;
+    }
+
+    public int getCt() {
+        return ct;
+    }
+
+    public static JobGroup getEnumType(String key) {
+        return Arrays.stream(JobGroup.values())
+                .filter(cmd -> cmd.name().equals(key))
+                .findFirst().orElse(JobGroup.UNKNOWN);
+    }
+
+    public static JobGroup getEnumType(int key) {
+        return Arrays.stream(JobGroup.values())
+                .filter(cmd -> cmd.ct == key)
+                .findFirst().orElse(JobGroup.UNKNOWN);
+    }
 }
