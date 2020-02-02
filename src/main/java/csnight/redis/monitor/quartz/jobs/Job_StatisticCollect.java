6b0bdddb05524e5abbd29e76967def1223b10988
CSNight <923739637@qq.com>
2020-02-02 18:38:54 +0800

    add job data test

--- src/main/java/csnight/redis/monitor/quartz/jobs/Job_StatisticCollect.java
+++ src/main/java/csnight/redis/monitor/quartz/jobs/Job_StatisticCollect.java
@@ -1,12 +1,19 @@
 package csnight.redis.monitor.quartz.jobs;
 
 import org.quartz.Job;
+import org.quartz.JobDataMap;
 import org.quartz.JobExecutionContext;
 import org.quartz.JobExecutionException;
 
+import java.util.Map;
+
 public class Job_StatisticCollect implements Job {
     @Override
-    public void execute(JobExecutionContext jobExecutionContext) throws JobExecutionException {
-        System.out.println("ssss");
+    public void execute(JobExecutionContext context) throws JobExecutionException {
+        JobDataMap jobDataMap = context.getJobDetail().getJobDataMap();
+        Map<String, String> params = (Map<String, String>) jobDataMap.get("params");
+        System.out.println(params.get("ins_id"));
+        System.out.println(params.get("cid"));
+        System.out.println(params.get("appId"));
     }
 }
