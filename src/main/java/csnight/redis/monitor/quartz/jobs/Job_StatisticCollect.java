ce51ec400db1a34b3b3ee115c3773f1fad612060
CSNight <923739637@qq.com>
2020-02-04 22:02:04 +0800

    add statistic job redis pool check and channel check

--- src/main/java/csnight/redis/monitor/quartz/jobs/Job_StatisticCollect.java
+++ src/main/java/csnight/redis/monitor/quartz/jobs/Job_StatisticCollect.java
@@ -1,5 +1,14 @@
 package csnight.redis.monitor.quartz.jobs;
 
+import com.alibaba.fastjson.JSONObject;
+import csnight.redis.monitor.msg.MsgBus;
+import csnight.redis.monitor.msg.entity.ChannelEntity;
+import csnight.redis.monitor.msg.entity.WssResponseEntity;
+import csnight.redis.monitor.msg.series.ResponseMsgType;
+import csnight.redis.monitor.redis.pool.MultiRedisPool;
+import csnight.redis.monitor.redis.pool.RedisPoolInstance;
+import csnight.redis.monitor.redis.statistic.InfoCmdParser;
+import csnight.redis.monitor.websocket.WebSocketServer;
 import org.quartz.Job;
 import org.quartz.JobDataMap;
 import org.quartz.JobExecutionContext;
@@ -12,8 +21,18 @@ public class Job_StatisticCollect implements Job {
     public void execute(JobExecutionContext context) throws JobExecutionException {
         JobDataMap jobDataMap = context.getJobDetail().getJobDataMap();
         Map<String, String> params = (Map<String, String>) jobDataMap.get("params");
-        System.out.println(params.get("ins_id"));
-        System.out.println(params.get("cid"));
-        System.out.println(params.get("appId"));
+        RedisPoolInstance pool = MultiRedisPool.getInstance().getPool(params.get("ins_id"));
+        if (pool == null) {
+            return;
+        }
+        Map<String, Map<String, String>> infos = InfoCmdParser.GetInfoAll(pool);
+        System.out.println(JSONObject.toJSONString(infos));
+        ChannelEntity che = MsgBus.getIns().getChannels().get(params.get("cid"));
+        if (che == null || params.get("appId").equals("")) {
+            return;
+        }
+        WssResponseEntity wre = new WssResponseEntity(ResponseMsgType.RMS_STAT, infos);
+        wre.setAppId(params.get("appId"));
+        WebSocketServer.getInstance().send(JSONObject.toJSONString(wre), che.getChannel());
     }
 }
