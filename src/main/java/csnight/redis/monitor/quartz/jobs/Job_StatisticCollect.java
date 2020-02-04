28cc16c0e2b8636fd5aafc26087faab303a44657
CSNight <923739637@qq.com>
2020-02-04 23:10:02 +0800

    add info sections parser

--- src/main/java/csnight/redis/monitor/quartz/jobs/Job_StatisticCollect.java
+++ src/main/java/csnight/redis/monitor/quartz/jobs/Job_StatisticCollect.java
@@ -1,19 +1,21 @@
 package csnight.redis.monitor.quartz.jobs;
 
 import com.alibaba.fastjson.JSONObject;
+import com.csnight.jedisql.JediSQL;
 import csnight.redis.monitor.msg.MsgBus;
 import csnight.redis.monitor.msg.entity.ChannelEntity;
 import csnight.redis.monitor.msg.entity.WssResponseEntity;
 import csnight.redis.monitor.msg.series.ResponseMsgType;
 import csnight.redis.monitor.redis.pool.MultiRedisPool;
 import csnight.redis.monitor.redis.pool.RedisPoolInstance;
-import csnight.redis.monitor.redis.statistic.InfoCmdParser;
+import csnight.redis.monitor.utils.IdentifyUtils;
 import csnight.redis.monitor.websocket.WebSocketServer;
 import org.quartz.Job;
 import org.quartz.JobDataMap;
 import org.quartz.JobExecutionContext;
 import org.quartz.JobExecutionException;
 
+import java.util.HashMap;
 import java.util.Map;
 
 public class Job_StatisticCollect implements Job {
@@ -25,8 +27,32 @@ public class Job_StatisticCollect implements Job {
         if (pool == null) {
             return;
         }
-        Map<String, Map<String, String>> infos = InfoCmdParser.GetInfoAll(pool);
-        System.out.println(JSONObject.toJSONString(infos));
+        String clientId = IdentifyUtils.getUUID();
+        JediSQL jediSQL = pool.getJedis(clientId);
+        String infos = jediSQL.info();
+        String[] sections = infos.replaceAll(" ", "").split("#");
+        Map<String, Map<String, String>> parts = new HashMap<>();
+        for (String section : sections) {
+            if (section.equals("")) {
+                continue;
+            }
+            String[] sec = section.split("\r\n");
+            Map<String, String> items = new HashMap<>();
+            for (int j = 1; j < sec.length; j++) {
+                if (sec[j].equals("")) {
+                    continue;
+                }
+                String[] kv = sec[j].split(":");
+                if (sec[j].contains("executable:") || sec[j].contains("config_file:")) {
+                    items.put(kv[0], sec[j].substring(kv[0].length() + 1));
+                } else {
+                    items.put(kv[0], String.valueOf(kv[1]));
+                }
+            }
+            parts.put(sec[0], items);
+        }
+        System.out.println(JSONObject.toJSONString(parts));
+        pool.close(clientId);
         ChannelEntity che = MsgBus.getIns().getChannels().get(params.get("cid"));
         if (che == null || params.get("appId").equals("")) {
             return;
