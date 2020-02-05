92dc42a4a17fa0f64e32b9e3f8527312c0b5cdc4
CSNight <923739637@qq.com>
2020-02-05 23:56:25 +0800

    add physical,commands,clients, parser

--- src/main/java/csnight/redis/monitor/quartz/jobs/Job_StatisticCollect.java
+++ src/main/java/csnight/redis/monitor/quartz/jobs/Job_StatisticCollect.java
@@ -2,6 +2,9 @@ package csnight.redis.monitor.quartz.jobs;
 
 import com.alibaba.fastjson.JSONObject;
 import com.csnight.jedisql.JediSQL;
+import csnight.redis.monitor.db.jpa.RmsRcsLog;
+import csnight.redis.monitor.db.jpa.RmsRosLog;
+import csnight.redis.monitor.db.jpa.RmsRpsLog;
 import csnight.redis.monitor.msg.MsgBus;
 import csnight.redis.monitor.msg.entity.ChannelEntity;
 import csnight.redis.monitor.msg.entity.WssResponseEntity;
@@ -15,6 +18,7 @@ import org.quartz.JobDataMap;
 import org.quartz.JobExecutionContext;
 import org.quartz.JobExecutionException;
 
+import java.util.Date;
 import java.util.HashMap;
 import java.util.Map;
 
@@ -30,7 +34,9 @@ public class Job_StatisticCollect implements Job {
         String clientId = IdentifyUtils.getUUID();
         JediSQL jediSQL = pool.getJedis(clientId);
         String infos = jediSQL.info();
+        String cmdInfos = jediSQL.info("commandstats");
         String[] sections = infos.replaceAll(" ", "").split("#");
+        String[] cmd_stats = cmdInfos.replace("# Commandstats\r\n", "").split("\r\n");
         Map<String, Map<String, String>> parts = new HashMap<>();
         for (String section : sections) {
             if (section.equals("")) {
@@ -51,8 +57,12 @@ public class Job_StatisticCollect implements Job {
             }
             parts.put(sec[0], items);
         }
-        System.out.println(JSONObject.toJSONString(parts));
         pool.close(clientId);
+        long tm = System.currentTimeMillis();
+        RmsRpsLog rpsLog = GetPhysicalStat(tm, params.get("ins_id"), parts);
+        RmsRosLog rosLog = GetCommandStat(tm, params.get("ins_id"), parts, cmd_stats);
+        RmsRcsLog rcsLog = GetClientStat(tm, params.get("ins_id"), parts);
+        System.out.println(JSONObject.toJSONString(rosLog));
         ChannelEntity che = MsgBus.getIns().getChannels().get(params.get("cid"));
         if (che == null || params.get("appId").equals("")) {
             return;
@@ -60,5 +70,65 @@ public class Job_StatisticCollect implements Job {
         WssResponseEntity wre = new WssResponseEntity(ResponseMsgType.RMS_STAT, infos);
         wre.setAppId(params.get("appId"));
         WebSocketServer.getInstance().send(JSONObject.toJSONString(wre), che.getChannel());
+    }
+
+    private RmsRpsLog GetPhysicalStat(long tm, String ins_id, Map<String, Map<String, String>> sections) {
+        RmsRpsLog rpsLog = new RmsRpsLog();
+        rpsLog.setTm(new Date(tm));
+        rpsLog.setIns_id(ins_id);
+        Map<String, String> memory = sections.get("Memory");
+        Map<String, String> cpu = sections.get("CPU");
+        Map<String, String> stats = sections.get("Stats");
+        rpsLog.setMem_us(Long.parseLong(memory.get("used_memory")));
+        rpsLog.setMem_rs(Long.parseLong(memory.get("used_memory_rss")));
+        rpsLog.setMem_peak(Long.parseLong(memory.get("used_memory_peak")));
+        rpsLog.setMem_oh(Long.parseLong(memory.get("used_memory_overhead")));
+        rpsLog.setMem_ds(Long.parseLong(memory.get("used_memory_dataset")));
+        rpsLog.setMem_fr(Double.parseDouble(memory.get("mem_fragmentation_ratio")));
+        rpsLog.setMem_frb(memory.containsKey("mem_fragmentation_bytes") ? Long.parseLong(memory.get("mem_fragmentation_bytes")) : 0L);
+        rpsLog.setCpu_su(Double.parseDouble(cpu.get("used_cpu_sys")));
+        rpsLog.setCpu_uu(Double.parseDouble(cpu.get("used_cpu_user")));
+        rpsLog.setIoi(Long.parseLong(stats.get("total_net_input_bytes")));
+        rpsLog.setIoo(Long.parseLong(stats.get("total_net_output_bytes")));
+        rpsLog.setIo_iik(Double.parseDouble(stats.get("instantaneous_input_kbps")));
+        rpsLog.setIo_iok(Double.parseDouble(stats.get("instantaneous_output_kbps")));
+        return rpsLog;
+    }
+
+    private RmsRosLog GetCommandStat(long tm, String ins_id, Map<String, Map<String, String>> sections, String[] cmd_stat) {
+        RmsRosLog rosLog = new RmsRosLog();
+        rosLog.setTm(new Date(tm));
+        rosLog.setIns_id(ins_id);
+        Map<String, String> stats = sections.get("Stats");
+        rosLog.setTcs(Long.parseLong(stats.get("total_commands_processed")));
+        rosLog.setOps(Long.parseLong(stats.get("instantaneous_ops_per_sec")));
+        rosLog.setPub_ch(Integer.parseInt(stats.get("pubsub_channels")));
+        rosLog.setPub_pat(Integer.parseInt(stats.get("pubsub_patterns")));
+        JSONObject joCmds = new JSONObject();
+        for (String stat : cmd_stat) {
+            JSONObject joCmd = new JSONObject();
+            String[] cmd = stat.replace("cmdstat_", "").split(":");
+            String[] infos = cmd[1].split(",");
+            for (String info : infos) {
+                String[] part = info.split("=");
+                joCmd.put(part[0], part[1]);
+            }
+            joCmds.put(cmd[0], joCmd);
+        }
+        rosLog.setCmd_stat(joCmds.toJSONString());
+        return rosLog;
+    }
+
+    private RmsRcsLog GetClientStat(long tm, String ins_id, Map<String, Map<String, String>> sections) {
+        RmsRcsLog rcsLog = new RmsRcsLog();
+        rcsLog.setTm(new Date(tm));
+        rcsLog.setIns_id(ins_id);
+        Map<String, String> stats = sections.get("Stats");
+        Map<String, String> clients = sections.get("Clients");
+        rcsLog.setTotal_cons_rec(Long.parseLong(stats.get("total_connections_received")));
+        rcsLog.setCli_blo(Integer.parseInt(clients.get("blocked_clients")));
+        rcsLog.setCli_con(Integer.parseInt(clients.get("connected_clients")));
+        rcsLog.setReject_cons(Long.parseLong(stats.get("rejected_connections")));
+        return rcsLog;
     }
 }
