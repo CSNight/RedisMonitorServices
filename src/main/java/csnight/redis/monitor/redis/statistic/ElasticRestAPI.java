c94743de3b23401a4609dbf6a4bf0ef727a1d8dc
CSNight <923739637@qq.com>
2020-02-08 23:01:19 +0800

    add es rest api

---
+++ src/main/java/csnight/redis/monitor/redis/statistic/ElasticRestAPI.java
@@ -0,0 +1,248 @@
+package csnight.redis.monitor.redis.statistic;
+
+import com.alibaba.fastjson.JSONObject;
+import org.apache.http.HttpHost;
+import org.elasticsearch.action.admin.indices.delete.DeleteIndexRequest;
+import org.elasticsearch.action.bulk.BackoffPolicy;
+import org.elasticsearch.action.bulk.BulkProcessor;
+import org.elasticsearch.action.bulk.BulkRequest;
+import org.elasticsearch.action.bulk.BulkResponse;
+import org.elasticsearch.action.support.master.AcknowledgedResponse;
+import org.elasticsearch.client.RequestOptions;
+import org.elasticsearch.client.RestClient;
+import org.elasticsearch.client.RestHighLevelClient;
+import org.elasticsearch.client.indices.CreateIndexRequest;
+import org.elasticsearch.client.indices.GetIndexRequest;
+import org.elasticsearch.client.indices.PutMappingRequest;
+import org.elasticsearch.common.Strings;
+import org.elasticsearch.common.settings.Settings;
+import org.elasticsearch.common.unit.TimeValue;
+import org.elasticsearch.common.xcontent.XContentBuilder;
+import org.elasticsearch.common.xcontent.XContentFactory;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.io.IOException;
+import java.math.BigDecimal;
+import java.math.RoundingMode;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Objects;
+import java.util.TimeZone;
+import java.util.concurrent.TimeUnit;
+
+public class ElasticRestAPI {
+    private static Logger _log = LoggerFactory.getLogger(ElasticRestAPI.class);
+    private RestHighLevelClient client;
+    private static JSONObject es_conf;
+    private BulkProcessor bulkProcessor;
+
+
+    public ElasticRestAPI() {
+        ConnectToES();
+    }
+
+    public BulkProcessor getBulk() {
+        return bulkProcessor;
+    }
+
+    public void ConnectToES() {
+        client = new RestHighLevelClient(RestClient.builder(new HttpHost("localhost", 9200, "http")));
+    }
+
+    public void CloseES() {
+        if (client != null) {
+            try {
+                client.close();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+            client = null;
+        }
+    }
+
+    public boolean SetIndices(String indices_name) {
+        try {
+            boolean index_status = createIndex(indices_name);
+            boolean mapping_status = createMapping(indices_name, GenerateMappingByJson());
+            return index_status && mapping_status;
+        } catch (Exception ex) {
+            return false;
+        }
+    }
+
+    public boolean indexExists(String indexName) {
+        GetIndexRequest request = new GetIndexRequest(indexName);
+        request.local(false);
+        request.humanReadable(true);
+        request.includeDefaults(true);
+        try {
+            return client.indices().exists(request, RequestOptions.DEFAULT);
+        } catch (IOException e) {
+            return false;
+        }
+    }
+
+    private boolean createIndex(String indexName) {
+        try {
+            GetIndexRequest request = new GetIndexRequest(indexName);
+            request.local(false);
+            request.humanReadable(true);
+            request.includeDefaults(true);
+            if (!indexExists(indexName)) {
+                CreateIndexRequest createIndexRequest = new CreateIndexRequest(indexName);
+                createIndexRequest.settings(Settings.builder().put("index.number_of_shards", 5).build());
+                client.indices().create(createIndexRequest, RequestOptions.DEFAULT);
+                return true;
+            } else {
+                System.out.println(String.format("index:%s already exists", indexName));
+                return false;
+            }
+        } catch (Exception ex) {
+            return false;
+        }
+    }
+
+    public boolean deleteIndex(String indexName) {
+        try {
+            if (indexExists(indexName)) {
+                DeleteIndexRequest deleteIndexRequest = new DeleteIndexRequest(indexName);
+                AcknowledgedResponse response = client.indices().delete(deleteIndexRequest, RequestOptions.DEFAULT);
+                return response.isAcknowledged();
+            } else {
+                System.out.println(String.format("index:%s does not exists", indexName));
+                return false;
+            }
+        } catch (Exception ex) {
+            return false;
+        }
+    }
+
+    private boolean createMapping(String indexName, XContentBuilder mappingContent) {
+        try {
+            PutMappingRequest mappingRequest = new PutMappingRequest(indexName);
+            mappingRequest.source(mappingContent);
+            mappingRequest.source(mappingContent);
+            return client.indices().putMapping(mappingRequest, RequestOptions.DEFAULT).isAcknowledged();
+        } catch (Exception ex) {
+            return false;
+        }
+    }
+
+    private XContentBuilder GenerateMappingByJson() {
+        JSONObject jo_index = es_conf.getJSONObject("");
+        XContentBuilder builder = null;
+        try {
+            builder = XContentFactory.jsonBuilder().startObject().startObject(es_conf.getString("doc_type")).startObject("properties");
+            for (Object k : jo_index.keySet()) {
+                try {
+                    builder = builder.startObject(k.toString()).field("type", jo_index.getString(k.toString())).endObject();
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+            builder.endObject().endObject().endObject();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        System.out.println(Strings.toString(Objects.requireNonNull(builder)));
+        return builder;
+    }
+
+    public XContentBuilder GenerateDocByPojo(String frame, String MappingType) {
+        XContentBuilder builder = null;
+        try {
+            builder = XContentFactory.jsonBuilder().startObject();
+            JSONObject bean = JSONObject.parseObject(frame);
+            JSONObject mapping = es_conf.getJSONObject(MappingType);
+            for (String v : mapping.keySet()) {
+                switch (mapping.getString(v)) {
+                    case "keyword":
+                    case "text":
+                        builder.field(v, bean.getString(v).equals("null") ? "" : bean.getString(v));
+                        break;
+                    case "double":
+                        BigDecimal big = BigDecimal.valueOf(bean.getDouble(v)).setScale(13, RoundingMode.UP);
+                        builder.field(v, big.doubleValue());
+                        break;
+                    case "integer":
+                        builder.field(v, bean.getIntValue(v));
+                        break;
+                    case "long":
+                        builder.field(v, bean.getLong(v));
+                        break;
+                    case "boolean":
+                        builder.field(v, bean.getBooleanValue(v));
+                        break;
+                    case "date":
+                        SimpleDateFormat toFormat = new SimpleDateFormat("yyyy-MM-dd'T'HH:mm:ss'Z'");
+                        SimpleDateFormat fromFormat = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
+                        toFormat.setTimeZone((TimeZone.getDefault()));
+                        builder.field(v, toFormat.format(fromFormat.parse(bean.getString("time_ms"))));
+                        break;
+                    case "geo_point":
+                        double x = bean.getDouble("x");
+                        double y = bean.getDouble("y");
+                        boolean illegal = coordinate_check(x, y);
+                        x = illegal ? x : 0;
+                        y = illegal ? y : 0;
+                        builder.startObject(v).field("lat", y).field("lon", x).endObject();
+                        break;
+                    default:
+                        builder.field(v, bean.getString(v) == null ? "" : bean.getString(v));
+                        break;
+                }
+            }
+            builder.endObject();
+            bean = null;
+        } catch (IOException | ParseException e) {
+            e.printStackTrace();
+        }
+        //system.out.println(Strings.toString(Objects.requireNonNull(builder)));
+        return builder;
+    }
+
+    private boolean coordinate_check(double x, double y) {
+        return x > 0 && x < 180 && y > 0 && y < 90;
+    }
+
+    public void ParallelBulkInitialize() {
+        BulkProcessor.Listener listener = new BulkProcessor.Listener() {
+            @Override
+            public void beforeBulk(long executionId, BulkRequest request) {
+                int numberOfActions = request.numberOfActions();
+                _log.info("Executing bulk [{}] with {} requests", executionId, numberOfActions);
+            }
+
+            @Override
+            public void afterBulk(long executionId, BulkRequest request, BulkResponse response) {
+                if (response.hasFailures()) {
+                    _log.warn("Bulk [{}] executed with failures", executionId);
+                } else {
+                    _log.info("Bulk [{}] completed in {} milliseconds", executionId, response.getTook().getMillis());
+                }
+            }
+
+            @Override
+            public void afterBulk(long executionId, BulkRequest request, Throwable failure) {
+                _log.error("Failed to execute bulk", failure);
+            }
+        };
+        BulkProcessor.Builder builder = BulkProcessor.builder((request, bulkListener) ->
+                client.bulkAsync(request, RequestOptions.DEFAULT, bulkListener), listener);
+        builder.setBulkActions(500);
+        builder.setConcurrentRequests(10);
+        builder.setFlushInterval(TimeValue.timeValueSeconds(10L));
+        builder.setBackoffPolicy(BackoffPolicy.constantBackoff(TimeValue.timeValueSeconds(2L), 3));
+        bulkProcessor = builder.build();
+    }
+
+    public boolean CloseBulkProcessor() {
+        try {
+            return bulkProcessor.awaitClose(2, TimeUnit.SECONDS);
+        } catch (InterruptedException e) {
+            bulkProcessor.close();
+            return false;
+        }
+    }
+}
