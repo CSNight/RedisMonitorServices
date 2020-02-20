b3b4a6d07124af203963f6f38fe7f35ed6d5b4a4
csnight <923739637@qq.com>
2020-02-20 23:32:32 +0800

    add async mail send and result save

--- src/main/java/csnight/redis/monitor/busi/sys/MailSendService.java
+++ src/main/java/csnight/redis/monitor/busi/sys/MailSendService.java
@@ -2,11 +2,14 @@ package csnight.redis.monitor.busi.sys;
 
 import com.sun.mail.util.MailSSLSocketFactory;
 import csnight.redis.monitor.db.jpa.SysMailConfig;
+import csnight.redis.monitor.db.jpa.SysMailRecord;
 import csnight.redis.monitor.db.repos.SysMailConfRepository;
+import csnight.redis.monitor.db.repos.SysMailRecRepository;
 import csnight.redis.monitor.db.repos.SysUserRepository;
 import csnight.redis.monitor.rest.sys.dto.MailConfDto;
 import csnight.redis.monitor.rest.sys.dto.MailSendDto;
 import csnight.redis.monitor.utils.BaseUtils;
+import csnight.redis.monitor.utils.IdentifyUtils;
 import org.springframework.mail.javamail.JavaMailSenderImpl;
 import org.springframework.mail.javamail.MimeMessageHelper;
 import org.springframework.stereotype.Service;
@@ -17,16 +20,25 @@ import javax.mail.MessagingException;
 import javax.mail.PasswordAuthentication;
 import javax.mail.Session;
 import javax.mail.internet.MimeMessage;
+import javax.net.ssl.KeyManagerFactory;
+import java.io.UnsupportedEncodingException;
 import java.security.GeneralSecurityException;
+import java.util.Date;
 import java.util.List;
 import java.util.Properties;
+import java.util.concurrent.CompletableFuture;
 
 @Service
+
 public class MailSendService {
     @Resource
     private SysUserRepository userRepository;
     @Resource
     private SysMailConfRepository mailConfRepository;
+    @Resource
+    private SysMailRecRepository mailRecRepository;
+    @Resource
+    private KeyManagerFactory kmf;
 
     public List<SysMailConfig> GetAllMailConfig() {
         return mailConfRepository.findAll();
@@ -51,6 +63,11 @@ public class MailSendService {
         mailConfig.setUid(uid);
         mailConfig.setProtocol("smtp");
         mailConfig.setEncode(dto.getEncode());
+        if (dto.getReceivers().size() > 0) {
+            mailConfig.setContact(String.join(";", dto.getReceivers().toArray(new String[]{})));
+        } else {
+            mailConfig.setContact("");
+        }
         return mailConfRepository.save(mailConfig);
     }
 
@@ -68,24 +85,62 @@ public class MailSendService {
         String uid = userRepository.findIdByUsername(BaseUtils.GetUserFromContext());
         SysMailConfig mailConfig = mailConfRepository.findByUid(uid);
         if (mailConfig != null) {
-            try {
-                Session session = initProperties(mailConfig);
-                JavaMailSenderImpl mailSender = new JavaMailSenderImpl();
-                mailSender.setSession(session);
-                MimeMessage mailMessage = mailSender.createMimeMessage();
-                MimeMessageHelper helper = new MimeMessageHelper(mailMessage, true,mailConfig.getEncode());
-                helper.setValidateAddresses(true);
-                helper.setFrom(mailConfig.getEmail());
-                helper.setTo(dto.getToList().toArray(new String[]{}));
-                helper.setSubject(dto.getSubject());
-                helper.setText("", dto.getContent());
-                mailSender.send(mailMessage);
-                return "success";
-            } catch (MessagingException e) {
-                return e.getMessage();
-            }
+            SysMailRecord record = new SysMailRecord();
+            record.setEmail(mailConfig.getEmail());
+            record.setFlag(IdentifyUtils.string2MD5(dto.getContent(), ""));
+            record.setTheme(dto.getSubject());
+            record.setTos(String.join(";", dto.getToList().toArray(new String[]{})));
+            record.setCreate_time(new Date());
+            record.setCreate_user(BaseUtils.GetUserFromContext());
+            record.setStatus("sending");
+            SysMailRecord sendRec = mailRecRepository.save(record);
+            CompletableFuture.supplyAsync(() -> send(mailConfig, dto))
+                    .exceptionally(this::getDepthCause)
+                    .thenApplyAsync((result) -> {
+                        if (result.equals("success")) {
+                            sendRec.setStatus("success");
+                        } else {
+                            sendRec.setStatus("failed");
+                            sendRec.setReason(result);
+                        }
+                        return mailRecRepository.save(sendRec);
+                    });
+            return "success";
         }
         return "config not found";
+    }
+
+    public String send(SysMailConfig mailConfig, MailSendDto dto) {
+        try {
+            Session session = initProperties(mailConfig);
+            JavaMailSenderImpl mailSender = new JavaMailSenderImpl();
+            mailSender.setDefaultEncoding(mailConfig.getEncode());
+            mailSender.setSession(session);
+            MimeMessage mailMessage = mailSender.createMimeMessage();
+            MimeMessageHelper helper = new MimeMessageHelper(mailMessage, true, mailConfig.getEncode());
+            helper.setValidateAddresses(true);
+            helper.setFrom(mailConfig.getEmail(), "RedisMonitorService");
+            helper.setTo(dto.getToList().toArray(new String[]{}));
+            if (dto.getCcList().size() > 0) {
+                helper.setCc(dto.getCcList().toArray(new String[]{}));
+            }
+            helper.setSubject(dto.getSubject());
+            helper.setText("", dto.getContent());
+            mailSender.send(mailMessage);
+        } catch (MessagingException | UnsupportedEncodingException e) {
+            return getDepthCause(e);
+        }
+        return "success";
+    }
+
+    private String getDepthCause(Throwable ex) {
+        String emsg = "";
+        Throwable t = ex.getCause();
+        while (t != null) {
+            emsg = t.getMessage();
+            t = t.getCause();
+        }
+        return emsg;
     }
 
     private Session initProperties(SysMailConfig conf) {
@@ -94,17 +149,19 @@ public class MailSendService {
         properties.setProperty("mail.smtp.host", conf.getHost());
         properties.setProperty("mail.smtp.port", String.valueOf(conf.getPort()));
         // 使用smtp身份验证
-        properties.put("mail.smtp.auth", "true");
-        // 使用SSL,企业邮箱必需 start
-        // 开启安全协议
         MailSSLSocketFactory mailSSLSocketFactory = null;
         try {
             mailSSLSocketFactory = new MailSSLSocketFactory();
             mailSSLSocketFactory.setTrustAllHosts(true);
+            mailSSLSocketFactory.setKeyManagers(kmf.getKeyManagers());
         } catch (GeneralSecurityException e) {
             e.printStackTrace();
         }
+        properties.put("mail.smtp.auth", "true");
         properties.put("mail.smtp.enable", "true");
+        properties.put("mail.smtp.ssl.enable", "true");
+        properties.put("mail.smtp.starttls.enable", "true");
+        properties.put("mail.smtp.starttls.required", "true");
         properties.put("mail.smtp.ssl.socketFactory", mailSSLSocketFactory);
         properties.put("mail.smtp.socketFactory.class", "javax.net.ssl.SSLSocketFactory");
         properties.put("mail.smtp.socketFactory.fallback", "false");
@@ -115,8 +172,6 @@ public class MailSendService {
                 return new PasswordAuthentication(conf.getEmail(), conf.getPwd());
             }
         });
-        // 使用SSL,企业邮箱必需 end
-        // TODO 显示debug信息 正式环境注释掉
         session.setDebug(false);
         return session;
     }
