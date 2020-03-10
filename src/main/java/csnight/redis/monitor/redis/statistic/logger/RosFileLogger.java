21cba91445e54e30c961a19f4a29fe720d77524d
csnight <923739637@qq.com>
2020-03-10 12:48:18 +0800

    reformat code

--- src/main/java/csnight/redis/monitor/redis/statistic/logger/RosFileLogger.java
+++ src/main/java/csnight/redis/monitor/redis/statistic/logger/RosFileLogger.java
@@ -5,6 +5,7 @@ import org.slf4j.LoggerFactory;
 
 public class RosFileLogger {
     private Logger _log = LoggerFactory.getLogger(RosFileLogger.class);
+
     public void log(String body) {
         _log.info(body);
     }
