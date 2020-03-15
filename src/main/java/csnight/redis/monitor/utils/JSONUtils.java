5c651fb8375c6e09476a27e001b7726d96cbee80
csnight <923739637@qq.com>
2020-03-15 17:54:56 +0800

    remove unused xml lib

--- src/main/java/csnight/redis/monitor/utils/JSONUtils.java
+++ src/main/java/csnight/redis/monitor/utils/JSONUtils.java
@@ -2,10 +2,7 @@ package csnight.redis.monitor.utils;
 
 import com.alibaba.fastjson.JSONArray;
 import com.alibaba.fastjson.JSONObject;
-import org.dom4j.Attribute;
-import org.dom4j.DocumentException;
-import org.dom4j.DocumentHelper;
-import org.dom4j.Element;
+
 
 import java.util.List;
 import java.util.Map;
@@ -44,51 +41,6 @@ public class JSONUtils {
      */
     public static String map2json(Map<?, ?> map) {
         return JSONObject.toJSONString(map);
-    }
-
-    /**
-     * 将xml字符串转换为JSON字符串
-     *
-     * @param xmlString xml字符串
-     * @return JSON对象
-     */
-    static String xml2json(String xmlString) {
-        try {
-            Element node = DocumentHelper.parseText(xmlString).getRootElement();
-            return elements2Json(node).toJSONString();
-        } catch (DocumentException e) {
-            e.printStackTrace();
-        }
-        return null;
-    }
-
-    /**
-     * org.dom4j.Element 转  com.alibaba.fastjson.JSONObject
-     *
-     * @param node Element
-     * @return JSONObject
-     */
-    private static JSONObject elements2Json(Element node) {
-        JSONObject result = new JSONObject();
-        // 当前节点的名称、文本内容和属性
-        List<Attribute> listAttr = node.attributes();// 当前节点的所有属性的list
-        for (Attribute attr : listAttr) {// 遍历当前节点的所有属性
-            result.put(attr.getName(), attr.getValue());
-        }
-        // 递归遍历当前节点所有的子节点
-        List<Element> listElement = node.elements();// 所有一级子节点的list
-        if (!listElement.isEmpty()) {
-            for (Element e : listElement) {// 遍历所有一级子节点
-                if (e.attributes().isEmpty() && e.elements().isEmpty()) // 判断一级节点是否有属性和子节点
-                    result.put(e.getName(), e.getTextTrim());// 沒有则将当前节点作为上级节点的属性对待
-                else {
-                    if (!result.containsKey(e.getName())) // 判断父节点是否存在该一级节点名称的属性
-                        result.put(e.getName(), new JSONArray());// 没有则创建
-                    ((JSONArray) result.get(e.getName())).add(elements2Json(e));// 将该一级节点放入该节点名称的属性对应的值中
-                }
-            }
-        }
-        return result;
     }
 
 
